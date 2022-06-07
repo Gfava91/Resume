2630a03a0860273fac69101a4b8578f86d40ce3d
Gfava91 <gregoryf45@nycstudents.net>
2022-06-07 12:58:57 -0400

    a

--- my-project/tailwind.config.js
+++ my-project/tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: { cormorant_garamond: "'Cormorant Garamond'" },
     extend: {},
   },
   plugins: [],
